65c11946b0ee8e6f5835372f8e51ec0c1e0fe1a7
Steven Le Rouzic <steven.lerouzic@gmail.com>
2015-04-14 01:01:43 +0200

    removed test game

--- src/com/stevenlr/testgame/components/ColorComponent.java
+++
@@ -1,9 +0,0 @@
-package com.stevenlr.testgame.components;
-
-import com.stevenlr.gameframework.entitysystem.components.Component;
-import com.stevenlr.gameframework.graphics.Color;
-
-public class ColorComponent extends Component {
-
-	public Color color;
-}
--- src/com/stevenlr/testgame/components/MovementComponent.java
+++
@@ -1,9 +0,0 @@
-package com.stevenlr.testgame.components;
-
-import com.stevenlr.gameframework.entitysystem.components.Component;
-
-public class MovementComponent extends Component {
-
-	public float dx;
-	public float dy;
-}

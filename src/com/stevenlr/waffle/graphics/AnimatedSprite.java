7ae173ebb1910934f1eee230647c749bb6f1fdc2
Steven Le Rouzic <steven.lerouzic@gmail.com>
2015-04-16 00:04:23 +0200

    added animated sprites instancing

--- src/com/stevenlr/waffle/graphics/AnimatedSprite.java
+++ src/com/stevenlr/waffle/graphics/AnimatedSprite.java
@@ -9,12 +9,53 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AnimatedSprite implements IBlittable {
+public class AnimatedSprite {
 
 	private SpriteSheet _spritesheet;
-	private int _currentFrame = 0;
-	private float _time = 0;
 	private List<Frame> _frames = new ArrayList<Frame>();
+
+	public class Instance implements IBlittable {
+
+		private AnimatedSprite _sprite;
+		private int _currentFrame = 0;
+		private float _time = 0;
+
+		public Instance(AnimatedSprite sprite) {
+			_sprite = sprite;
+		}
+
+		public void update(float dt) {
+			if (dt * _time < 0) {
+				_time = -_time;
+			}
+			_time += dt;
+
+			while (_time > _sprite._frames.get(_currentFrame).duration) {
+				_time -= _sprite._frames.get(_currentFrame).duration;
+				_currentFrame = (_currentFrame + 1) % _sprite._frames.size();
+			}
+
+			while (_time < -_sprite._frames.get(_currentFrame).duration) {
+				_time += _sprite._frames.get(_currentFrame).duration;
+				_currentFrame = (_currentFrame + _sprite._frames.size() - 1) % _sprite._frames.size();
+			}
+		}
+
+		@Override
+		public int getWidth() {
+			return _sprite._spritesheet.getTileWidth();
+		}
+
+		@Override
+		public int getHeight() {
+			return _sprite._spritesheet.getTileHeight();
+		}
+
+		@Override
+		public void blitOn(Graphics2D graphics, int x, int y, int width, int height) {
+			_sprite._frames.get(_currentFrame).region.blitOn(graphics, x, y, width, height);
+		}
+	}
 
 	private class Frame {
 
@@ -31,43 +72,15 @@ public class AnimatedSprite implements IBlittable {
 		_spritesheet = new SpriteSheet(filename, tileWidth, tileHeight);
 	}
 
+	public Instance getInstance() {
+		return new Instance(this);
+	}
+
 	public void addFrame(float duration, int tile) {
 		addFrame(duration, tile % _spritesheet.getNumTilesX(), tile / _spritesheet.getNumTilesX());
 	}
 
 	public void addFrame(float duration, int tileX, int tileY) {
 		_frames.add(new Frame(duration, _spritesheet.getRegion(tileX, tileY)));
-	}
-
-	public void update(float dt) {
-		if (dt * _time < 0) {
-			_time = -_time;
-		}
-		_time += dt;
-
-		while (_time > _frames.get(_currentFrame).duration) {
-			_time -= _frames.get(_currentFrame).duration;
-			_currentFrame = (_currentFrame + 1) % _frames.size();
-		}
-
-		while (_time < -_frames.get(_currentFrame).duration) {
-			_time += _frames.get(_currentFrame).duration;
-			_currentFrame = (_currentFrame + _frames.size() - 1) % _frames.size();
-		}
-	}
-
-	@Override
-	public int getWidth() {
-		return _spritesheet.getTileWidth();
-	}
-
-	@Override
-	public int getHeight() {
-		return _spritesheet.getTileHeight();
-	}
-
-	@Override
-	public void blitOn(Graphics2D graphics, int x, int y, int width, int height) {
-		_frames.get(_currentFrame).region.blitOn(graphics, x, y, width, height);
 	}
 }
